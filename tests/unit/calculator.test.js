f5b94e4b4b66b36041839c5db10dc0391c367fb3
Daniel Gariani Rafael <danielgrafael@gmail.com>
2026-03-04 23:04:21 +0000

    adding database

---
+++ tests/unit/calculator.test.js
@@ -0,0 +1,11 @@
+const calc = require("../../models/calculator.js");
+
+test("sum", () => {
+  const result = calc.sum(2, 2);
+  expect(result).toBe(4);
+});
+
+test("sum", () => {
+  const result = calc.sum(2, 5);
+  expect(result).toBe(7);
+});
