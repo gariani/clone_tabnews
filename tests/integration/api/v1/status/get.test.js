b4409578bdf5ca9e45205612cdbbc2497d5bc820
Daniel Gariani Rafael <danielgrafael@gmail.com>
2026-04-15 21:07:48 +0000

    checks

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -21,7 +21,7 @@ test("GET to /api/v1/databaseVersion should return 16.0", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBoby = await response.json();
-  expect(responseBoby.db_version).toEqual("16.0");
+  expect(responseBoby.db_version).toMatch(/^16\./);
 });
 
 test("GET to /api/v1/status should return -1", async () => {
