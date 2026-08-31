7051b17ed12d6b449ab3e07a51646d969d8acc19
Daniel Gariani Rafael <danielgrafael@gmail.com>
2026-08-31 20:55:10 +0000

    fix: fixing the lint issues

--- infra/database.js
+++ infra/database.js
@@ -22,8 +22,6 @@ async function query(queryObject) {
 
     const result = await client.query(queryObject);
     return result;
-  } catch (error) {
-    throw error;
   } finally {
     if (client) await client.end();
   }
@@ -36,8 +34,6 @@ async function queryWithParam(queryObject, values) {
     client = await getNewClient();
     const result = await client.query(queryObject, values);
     return result;
-  } catch (error) {
-    throw error;
   } finally {
     if (client) await client.end();
   }
